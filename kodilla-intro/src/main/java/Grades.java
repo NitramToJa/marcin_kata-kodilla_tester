e5651065e951b7a2aeaea1bb508f5bd88e20993b
Marcin Kata <marcin.kata@interia.pl>
2020-09-16 20:41:27 +0200

    zadanie 2.4

--- kodilla-intro/src/main/java/Grades.java
+++ kodilla-intro/src/main/java/Grades.java
@@ -1,31 +1,30 @@
-/*public class Grades {
+public class Grades {
     private int[] grades;
     private int size;
 
     public Grades() {
         this.grades = new int[10];
         this.size = 0;
+
     }
 
     public void add(int value) {
-        if (this.size == 10){
+        if (this.size == 10) {
             return;
         }
         this.grades[this.size] = value;
         this.size++;
     }
 
-    private static String lastGrade(){
-        System.out.println("Ostatnia dodana ocena: " + grades[])
+    public int lastGrade(){
+        return this.grades[this.size -1];
     }
 
-    private static Integer gradesSum(){
-        int sum =0;
-        for (int i = 0; i < size; i++ ){
-            int sum = grades[i] + sum;
-            }
-        double avg = (double) sum / size;
-        System.out.println("Srednia z tablicy: " + avg);
+    public int gradesAverage() {
+        int sum = 0;
+        for (int i = 0; i < size; i++) {
+            sum += this.grades[i];
+        }
+        return sum / this.size;
+    }
 }
-
- */

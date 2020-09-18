adda587e69e730a08a6dfd4c263703820594dacf
Marcin Kata <marcin.kata@interia.pl>
2020-09-18 11:00:37 +0200

    zadanie 2.5 v2

--- kodilla-intro/src/main/java/RandomNumbers.java
+++ kodilla-intro/src/main/java/RandomNumbers.java
@@ -1,7 +1,16 @@
 /*public class RandomNumbers {
+
+    public void Random() {
     Random r = new Random();
+    int low = 0;
+    int high = 30;
+    int sum = 0;
 
-
+    if (sum <= 5000) {
+        int result = r.nextInt(high - low) + 1;
+        sum += result;
+    }
+    }
 }
+*/
 
- */
--- kodilla-intro/src/main/java/User.java
+++ kodilla-intro/src/main/java/User.java
@@ -4,7 +4,6 @@ public class User {
     public static void main(String[] args) {
         String userName;
         int userAge;
-        User[] users = {anna, betty, carl, david, eva, frankie};
 
         User anna = new User("Anna", 20);
         User betty = new User("Betty", 33);
@@ -12,8 +11,25 @@ public class User {
         User david = new User("David", 13);
         User eva = new User("Eva", 18);
         User frankie = new User("Frankie", 45);
+
+        User[] users = {anna, betty, carl, david, eva, frankie};
     }
 
+    public double ageAverage() {
+        int result = 0;
+        for (int i = 0; i < users.size; i++) {
+            result += users[i]; usersAge;
+        }
+        return result / users.size;
 
+    }
 
+    public void youngerThenAverage() {
+        for (int i = 0; i < users.size; i++) {
+            if (users[i],userAge<ageAverage ){
+                System.out.println(users[i]);
+                else
+            }
+        }
+    }
 }

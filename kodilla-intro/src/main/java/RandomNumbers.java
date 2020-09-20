d53b90dbaf55c7ffb3b82dacb0c1910f633143d8
Marcin Kata <marcin.kata@interia.pl>
2020-09-20 10:49:34 +0200

    zadaanie 2.5 losowanie v2

--- kodilla-intro/src/main/java/RandomNumbers.java
+++ kodilla-intro/src/main/java/RandomNumbers.java
@@ -4,7 +4,6 @@ public class RandomNumbers {
     private int[] randomNumbers;
     private int min;
     private int max;
-    private int sum = 0;
 
     public RandomNumbers() {
         this.randomNumbers = new int[5000];
@@ -13,20 +12,16 @@ public class RandomNumbers {
     public void randomizeNumbers() {
         int low = 0;
         int high = 30;
-        for (int i = 0; i < 5000; i++) {
-            Random r = new Random();
-            randomNumbers[i] = r.nextInt((high - low) + 1);
-            sum = sum + randomNumbers[i];
+        int sum = 0;
+        while (sum <= 5000) {
+            for (int i = 0; i < 5000; i++) {
+                Random r = new Random();
+                randomNumbers[i] = r.nextInt((high - low) + 1);
+                sum = sum + randomNumbers[i];
+            }
         }
     }
-
-    public void isSumTheEnd() {
-            if (sum <= 5000) {
-            this.randomizeNumbers ();
-            }
-
-        }
-    public int minValue () {
+        public int minValue () {
         min = randomNumbers[0];
         for (int i=0; i < randomNumbers.length; i++) {
             if (randomNumbers[i] < min) {
@@ -49,7 +44,6 @@ public class RandomNumbers {
     public static void main (String[] args) {
         RandomNumbers numberTable = new RandomNumbers();
         numberTable.randomizeNumbers();
-        numberTable.isSumTheEnd();
         System.out.println("Minimal randomize number is: " + numberTable.minValue());
         System.out.println("Maximal randomize number is: " + numberTable.maxValue());
     }

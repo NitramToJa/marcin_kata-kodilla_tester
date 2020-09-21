d3dc4660f87e5f805f43b3ffb1d55ffbed8392a6
Marcin Kata <marcin.kata@interia.pl>
2020-09-21 17:26:11 +0200

    zadanie 2.6

--- kodilla-intro/src/main/java/Application.java
+++ kodilla-intro/src/main/java/Application.java
@@ -1,15 +1,33 @@
 public class Application {
-    public static void main(String[] args) {
-        String name = "Adam";
-        double age = 40.5;
-        double height = 178;
+    private String name;
+    private double age;
+    private double height;
 
-        if (name != null) {
-            if (age > 30 && height > 160) {
-                System.out.println("User is older than 30 and higher then 160cm");
-            } else {
-                System.out.println("User is exactly 30 or younger than 30 or he is exactly 160cm or shorter than 160cm");
-            }
+    public Application(String name, double age, double height) {
+        this.name = name;
+        this.age = age;
+        this.height = height;
+    }
+
+    public void compareValues() {
+        if (this.name == null) {
+            return;
         }
+
+        if (this.age > 30 && this.height > 160) {
+            System.out.println(this.name + " is older than 30 and taller than 160 cm.");
+        } else {
+            System.out.println(this.name + " is younger than 30 or shorter than 160 cm.");
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Application adam = new Application("Adam", 40.5, 178);
+
+        System.out.println("Name: " + adam.name);
+        System.out.println("Age: " + adam.age);
+        System.out.println("Height: " + adam.height);
+        adam.compareValues();
     }
 }

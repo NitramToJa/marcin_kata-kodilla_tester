85ea9eecebcd68cc7d763b219843d62d27aba443
Marcin Kata <marcin.kata@interia.pl>
2020-09-28 21:10:59 +0200

    zadanie 3.2 dziedziczenieV2

--- kodilla-basic-tests/src/main/java/com/kodilla/inheritance/homework/MacOs.java
+++ kodilla-basic-tests/src/main/java/com/kodilla/inheritance/homework/MacOs.java
@@ -4,4 +4,12 @@ public class MacOs extends OperatingSystem{
     public MacOs(int realiseYear) {
         super(realiseYear);
     }
+    public void turnOn() {
+        System.out.println("macOs turned on");
+    }
+
+    public void turnOff() {
+        System.out.println("macOs turned off");
+    }
+
 }
--- kodilla-basic-tests/src/main/java/com/kodilla/inheritance/homework/Windows.java
+++ kodilla-basic-tests/src/main/java/com/kodilla/inheritance/homework/Windows.java
@@ -1,7 +1,15 @@
 package com.kodilla.inheritance.homework;
 
-public class Windows extends OperatingSystem{
+public class Windows extends OperatingSystem {
     public Windows(int realiseYear) {
         super(realiseYear);
+    }
+
+    public void turnOn() {
+        System.out.println("Windows turned on");
+    }
+
+    public void turnOff() {
+        System.out.println("Windows turned off");
     }
 }
